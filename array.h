3513d2de38cdd2c6ebcd18edca26d13c6a866ab0
Akshay Jha <akshayjha@live.in>
2016-09-28 23:56:20 +0530

    Update array.h

--- array.h
+++ array.h
@@ -22,15 +22,67 @@ CSE, 3rd sem
 #define lengthOf(array) (sizeof((array))/sizeof((array)[0]))
 typedef int BOOLEAN;
 
+void print_array(int *arr, int n);
+void input_array(int *arr, int n);
+void sort(int *arr, int n);
+void quicksort(int *A, int p, int r);
+int partition(int *A, int p, int r);
+void swap(int *a, int *b);
+
 void print_array(int *arr, int n)
 {
-	
+
 	for(int i=0; i<n; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 }
 
+void input_array(int *arr, int n)
+{
+	printf("Enter the elements: \n");
+	int i;
+	for (i=0; i<n; i++)
+		scanf("%d", &arr[i]);
+}
 
 
+void sort(int *arr, int n)
+{
+	quicksort(arr, 0, n-1);
+}
+
+
+void quicksort(int *A, int p, int r)
+{
+   if( p < r )
+   {
+      int q = partition(A, p, r);
+      quicksort(A, p, q-1);
+      quicksort(A, q+1, r);
+   }
+}
+
+int partition(int *A, int p, int r)
+{
+   int x = A[r];
+   int i = p-1;
+   for(int j=p; j <= r-1; j++)
+   {
+      if( A[j] <= x )
+      {
+         i++;
+         swap(&A[i], &A[j]);
+      }
+   }
+   swap(&A[i+1], &A[r]);
+   return (i+1);
+}
+
+void swap(int *a, int *b)
+{
+   int t = *a;
+   *a = *b;
+   *b = t;
+}
 
 #endif
